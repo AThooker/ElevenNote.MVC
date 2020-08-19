aa90e3c3ecad0b0430543ab85479aae0bb94ec10
AThooker <austintrent@icloud.com>
2020-08-19 10:37:28 -0400

    edit functionality

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -50,10 +50,44 @@ namespace ElevenNote.WebMVC.Controllers
         public ActionResult Details(int id)
         {
             var service = CreateNoteService();
-            var model = service.GetNoteById();
+            var model = service.GetNoteById(id);
             return View(model);
         }
 
+        //GET : Edit
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model = new NoteEdit
+            {
+                NoteId = detail.NoteId,
+                Title = detail.Title,
+                Content = detail.Content
+            };
+            return View(model);
+        }
+        //POST : Edit
+        [HttpPost, ActionName("Edit")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if(model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = CreateNoteService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated successfully.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View();
+        }
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
